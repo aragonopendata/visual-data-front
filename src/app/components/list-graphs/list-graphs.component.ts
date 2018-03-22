0a541094eb5dfab5de33833e71061bfe5d71e4fb
Julio <julio88.99@gmail.com>
2018-03-22 09:21:18 +0100

    list graph service

--- src/app/components/list-graphs/list-graphs.component.ts
+++ src/app/components/list-graphs/list-graphs.component.ts
@@ -2,8 +2,8 @@ import 'rxjs/add/operator/switchMap';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { Location } from '@angular/common';
-import { HttpClient } from '@angular/common/http';
 import { Router } from '@angular/router';
+import { ListGraphService } from '../../services/list-graph.service';
 
 @Component({
   selector: 'app-list-graphs',
@@ -11,15 +11,24 @@ import { Router } from '@angular/router';
   styleUrls: ['./list-graphs.component.css']
 })
 export class ListGraphsComponent implements OnInit {
+  charts: any;
+
   constructor(
     private route: ActivatedRoute,
     private location: Location,
-    private router: Router
-  ) {}
+    private router: Router,
+    private listGraphService: ListGraphService
+  ) {
+    this.charts = [];
+  }
 
-  ngOnInit(): void {}
+  ngOnInit() {
+    this.listGraphService.getCharts().subscribe(data => {
+      this.charts = data.charts;
+    });
+  }
 
-  goBack(): void {}
+  goBack() {}
 
   next() {
     this.router.navigate(['/selectData/']);
