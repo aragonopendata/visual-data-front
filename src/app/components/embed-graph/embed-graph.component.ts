7d8f98cde7552300d87777b6f4f2d86a9eec89fe
Julio <julio88.99@gmail.com>
2018-03-22 17:36:47 +0100

    Graph embed fix

--- src/app/components/embed-graph/embed-graph.component.ts
+++ src/app/components/embed-graph/embed-graph.component.ts
@@ -3,37 +3,31 @@ import { GraphService } from '../../services/graph.service';
 import { Router, ActivatedRoute } from '@angular/router';
 
 @Component({
-    selector: 'app-embed-graph',
-    templateUrl: './embed-graph.component.html',
-    styleUrls: ['./embed-graph.component.css']
+  selector: 'app-embed-graph',
+  templateUrl: './embed-graph.component.html',
+  styleUrls: ['./embed-graph.component.css']
 })
 export class EmbedGraphComponent implements OnInit {
-    // public chartData = [];
-    public chartLegend = true;
-    public chartOptions: any = {
-        responsive: true
-    };
-    public chartData: Array<any> = [
-        { data: [0], label: 'Series A' }
-    ];
-    public chartLabels: Array<any> = ['Select Data'];
-    public chartColors: Array<any> = [];
-    public chartType = 'bar';
+  // public chartData = [];
+  public chartLegend = true;
+  public chartOptions: any = {
+    responsive: true
+  };
 
-    constructor(private router: Router, private graphservice: GraphService, private activatedRoute: ActivatedRoute) {
-    }
+  public chart: any;
 
-    ngOnInit() {
-        this.graphservice.getChart(this.activatedRoute.snapshot.url[2]).subscribe(data => {
-            this.chartType = data.type;
-            console.log(this.chartType);
-            this.chartLabels = data.labels;
-            console.log(this.chartLabels);
-            this.chartColors = data.colors;
-            console.log(this.chartColors);
-            this.chartData = data.data;
-            console.log(this.chartData);
-        });
-    }
+  constructor(
+    private router: Router,
+    private graphservice: GraphService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
+  ngOnInit() {
+    this.graphservice
+      .getChart(this.activatedRoute.snapshot.url[2])
+      .subscribe(data => {
+        this.chart = data;
+        console.log(data);
+      });
+  }
 }
--- src/app/components/end-graph/end-graph.component.ts
+++ src/app/components/end-graph/end-graph.component.ts
@@ -6,51 +6,40 @@ import { Router } from '@angular/router';
 import { GraphService } from '../../services/graph.service';
 
 @Component({
-    selector: 'app-end-graph',
-    templateUrl: './end-graph.component.html',
-    styleUrls: ['./end-graph.component.css']
+  selector: 'app-end-graph',
+  templateUrl: './end-graph.component.html',
+  styleUrls: ['./end-graph.component.css']
 })
 export class EndGraphComponent implements OnInit {
-    public chartLegend = true;
-    public chartOptions: any = {
-        responsive: true
-    };
-    public chartData: Array<any> = [
-        { data: [0], label: 'Series A' }
-    ];
-    public chartLabels: Array<any> = ['Select Data'];
-    public chartColors: Array<any> = [];
-    public chartType = 'bar';
+  public chartLegend = true;
+  public chartOptions: any = {
+    responsive: true
+  };
 
-    constructor(
-        private activatedRoute: ActivatedRoute,
-        private location: Location,
-        private router: Router,
-        private graphservice: GraphService
-    ) {
-    }
+  public chart: any;
 
-    ngOnInit(): void {
-        let id;
-        const aux = this.activatedRoute.snapshot.url[0];
-        if (aux.toString() !== '/endGraphic') {
-            id = this.activatedRoute.snapshot.url[2];
-        } else {
-            id = this.activatedRoute.snapshot.url[1];
-        }
-        this.graphservice.getChart(id).subscribe(data => {
-            this.chartType = data.type;
-            console.log(this.chartType);
-            this.chartLabels = data.labels;
-            console.log(this.chartLabels);
-            this.chartColors = data.colors;
-            console.log(this.chartColors);
-            this.chartData = data.data;
-            console.log(this.chartData);
-        });
-    }
+  constructor(
+    private activatedRoute: ActivatedRoute,
+    private location: Location,
+    private router: Router,
+    private graphservice: GraphService
+  ) {}
 
-    goBack(): void {
-        this.location.back();
+  ngOnInit(): void {
+    let id;
+    const aux = this.activatedRoute.snapshot.url[0];
+    if (aux.toString() !== '/endGraphic') {
+      id = this.activatedRoute.snapshot.url[2];
+    } else {
+      id = this.activatedRoute.snapshot.url[1];
     }
+    this.graphservice.getChart(id).subscribe(data => {
+      this.chart = data;
+      console.log(data);
+    });
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
