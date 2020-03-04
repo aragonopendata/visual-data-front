2aa8100d870ddd639536db369a2a22d9023b2a73
rcancer-dtt <rcancer@deloitte.es>
2020-03-04 20:45:39 +0100

    Update: styles categories

--- src/app/components/home-focus/home-focus.component.ts
+++ src/app/components/home-focus/home-focus.component.ts
@@ -11,51 +11,37 @@ import { Constants } from '../../app.constants';
 
 export class HomeFocusComponent implements OnInit {
 
-  categoriesAPI: string[];
-  categoriesAll: string[];
-  categoriesReduce:string[];
-  moreCategories: boolean = false;
+  categoriesHidden: string[];
+  categoriesVisible:string[];
+  viewMoreCategories: boolean = false;
   routerLinkAddHistory: string;
 
   constructor(private historiesService: HistoriesService) { 
     this.routerLinkAddHistory = Constants.ROUTER_LINK_ADD_HISTORY;
-    this.categoriesAPI = [];
   }
 
   ngOnInit() {
-    // this.createEmptyVectors();
     this.getCategories();
   }
-
-  // createEmptyVectors(){
-  //   this.categoriesAPI = [];
-  // }
 
   getCategories(){
     this.historiesService.getCategories().subscribe(categories => {
 
-      // try {
-        let coreCategories = categories;
-        let positionCategories = coreCategories[0].indexOf(Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_NAME_CATEGORIES);
-        coreCategories.splice(0,1);
+      let categoriesAPI = [];
+      let coreCategories = categories;
+      let positionCategories = coreCategories[0].indexOf(Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_NAME_CATEGORIES);
+      coreCategories.splice(0,1);
 
-				for (let view of coreCategories) {
-          this.categoriesAPI.push(view[positionCategories]);
-        }
+      for (let view of coreCategories) {
+        categoriesAPI.push(view[positionCategories]);
+      }
 
-        this.categoriesAll=this.categoriesAPI;
-        this.categoriesReduce=this.categoriesAPI.slice(0,4);
-        
-			// } catch (error) {
-			// 	console.error('Error: getCategories()');
-      // }
-      
-		});
-  }
+      this.categoriesVisible = categoriesAPI.slice(0,4);
+      this.categoriesHidden = categoriesAPI.slice(4,categoriesAPI.length);
 
-  clickCategories(){
-    this.moreCategories=!this.moreCategories;
-
+		},err => {
+      console.log('Error al obtener las categorias');
+    });
   }
 
 }
