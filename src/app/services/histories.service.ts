7b3971437c3b30269766c9b482f0dd451c0ebef4
rcancer-dtt <rcancer@deloitte.es>
2020-03-04 22:53:10 +0100

    Updated: histories service with mocks

--- src/app/services/histories.service.ts
+++ src/app/services/histories.service.ts
@@ -2,34 +2,35 @@ import { Injectable } from '@angular/core';
 import { Http, URLSearchParams } from '@angular/http';
 import { Constants } from '../app.constants';
 import { map } from 'rxjs/operators';
-
-
+import { Observable } from 'rxjs';
+import { History } from '../models/History';
+import { Category } from '../models/Category';
 
 @Injectable()
 export class HistoriesService {
 
+  constructor(private http: Http) { }
 
-  constructor(private http: Http) {
-  }
-
-  
-  public getCategories() {
+  public getCategories(): Observable<Category[]> {
     let fullUrl=Constants.AOD_BASE_URL_LOCAL + Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC +Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_PREVIEW;
-    console.log(fullUrl);
     let params = new URLSearchParams();
     params.append(Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_FILTER_SQL, Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_SQL_NIVEL_1);
     params.append(Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_VIEW_ID, Constants.SERVER_API_LINK_GA_OD_CORE_PUBLIC_VIEW_ID_NUMBER_TOPICS);
-    console.log(params)
-    return this.http.get(fullUrl, { search: params }).pipe(map(res => res.json()));
+    return this.http.get(fullUrl, { search: params }).pipe(
+      map(data => {
+        return data.json().splice(1,data.json().length).map( element => {
+          return new Category(element[0],  element[1], element[2], element[3], element[4], element[5]);
+        });
+      })
+    );
   }
   
-  public getHistories(){
-    let fullUrl=Constants.AOD_BASE_URL_MOCK + Constants.MOCK_HISTORY ;
-    return this.http.get(fullUrl).pipe(map(res => res.json()));
+  public getHistories(): Observable<History[]>{
+    return this.http.get('assets/mocks/histories.json').map(res => res.json());
+    ;
   }
 
   public getHistory(id:string){
-    let fullUrl=Constants.AOD_BASE_URL_MOCK + Constants.MOCK_HISTORY ;
-    return this.http.get(fullUrl).pipe(map(res => res.json()));
+    return this.http.get('assets/mocks/histories.json').pipe(map(res => res.json()));
   }
 }
