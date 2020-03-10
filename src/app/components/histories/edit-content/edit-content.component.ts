f17d5b6859b333690882cb3c4ada752f430bbe05
rcancer-dtt <rcancer@deloitte.es>
2020-03-10 13:10:44 +0100

    Edit content form Angular

--- src/app/components/histories/edit-content/edit-content.component.ts
+++ src/app/components/histories/edit-content/edit-content.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 import { Router } from '@angular/router';
 import { VisualGrapsService } from '../../../services/visual-graps.service';
 import { Content } from '../../../models/History';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-content',
@@ -11,26 +12,41 @@ import { Content } from '../../../models/History';
 export class EditContentComponent implements OnInit {
 
   contentModel: Content = {}
+  contentForm: FormGroup;
 
   @Output() contentCreate = new EventEmitter<any>();
 
-  constructor( private _route: Router, private _servicio: VisualGrapsService) {  }
+  constructor( private _route: Router, private _servicio: VisualGrapsService, private _formBuilder:FormBuilder) {  }
 
   ngOnInit() {
+    this.initiateForm();
     this._servicio.getIdGraph().subscribe(id => {
       this.contentModel.idGraph=id;
-      console.log(id)
+      this.contentForm.controls['idGraph'].setValue(id);
     });
+    
   }
 
+  initiateForm(){
+    this.contentForm = this._formBuilder.group({
+      title: new FormControl('', Validators.required),
+      description: new FormControl('', Validators.required),
+      idGraph: new FormControl('', Validators.required)
+    })
+
+  }
   openVisualData() {
     document.getElementsByTagName('body')[0].classList.add('no-scroll');
     this._route.navigate([{outlets: {modal: 'visualData'}}]);
   }
 
   saveContent(){
+    this.contentModel = {title: this.contentForm.get('title').value, 
+                         description: this.contentForm.get('description').value, 
+                         idGraph: this.contentForm.get('idGraph').value};
     this.contentCreate.emit(this.contentModel);
     this.contentModel = {};
+    this.contentForm.reset();
   }
 
 }
