4696c70d967937b90801d6e86204397e8eb2dc1d
rcancer-dtt <rcancer@deloitte.es>
2020-03-31 16:10:00 +0200

    enum for types and aligns contents

---
+++ src/app/models/Aligns.ts
@@ -0,0 +1,4 @@
+export enum Aligns {
+    "Descripción a la derecha, fuente de datos a la izquierda" = 1,
+    "Descripción a la izquierda, fuente de datos a la derecha" = 2
+}
---
+++ src/app/models/TypesContents.ts
@@ -0,0 +1,5 @@
+export enum TypesContents {
+    "Gráfica" = 1 ,
+    "Enlace YouTube"= 2,
+    "Enlace Share Slides" = 3
+}
