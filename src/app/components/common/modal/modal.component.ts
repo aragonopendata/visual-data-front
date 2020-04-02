eff6a1503d940f9220de96b6145b2cb5898e7d11
Rosa Maria Cancer <rcancer@deloitte.com>
2020-04-02 18:48:45 +0200

    show popup visualdata

--- src/app/components/common/modal/modal.component.ts
+++ src/app/components/common/modal/modal.component.ts
@@ -1,16 +1,24 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Inject, OnDestroy } from '@angular/core';
 import { Router } from '@angular/router';
+import { DOCUMENT } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.scss']
 })
-export class ModalComponent implements OnInit {
+export class ModalComponent implements OnInit, OnDestroy{
 
-  constructor( private _route: Router ) { }
+  constructor(@Inject(DOCUMENT) document: any, private _route: Router ) { }
 
   ngOnInit() {
+    document.getElementsByTagName('body')[0].classList.add('no-scroll');
+
+  }
+
+  ngOnDestroy(){
+    document.getElementsByTagName('body')[0].classList.remove('no-scroll');
+
   }
 
   closeModal() {
